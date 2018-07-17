fb6011661f488f4bd46770c935c3f485104b015f
Srijayanth Sridhar <srijayanth@gmail.com>
2018-07-17 12:33:26 +0530

    Added a simple way to validate json schemas

---
+++ json_schemas/validate_against.js
@@ -0,0 +1,23 @@
+const Ajv=require("ajv");
+const ajv=new Ajv({verbose:true});
+const fs=require("fs");
+
+let fileName=process.argv[2] || "./sample.json";
+let schemaName=process.argv[3] || "./schema.json";
+
+let schema=JSON.parse(fs.readFileSync(schemaName,"utf8"));
+let json=JSON.parse(fs.readFileSync(fileName,"utf8"));
+
+let validate=ajv.compile(schema);
+let result=validate(json);
+
+if(result) {
+  console.log(`${fileName} successfully validated against ${schemaName}`);
+  process.exit(0);
+}
+
+validate.errors.forEach(({keyword,dataPath,message,data})=>{
+  console.log(`${dataPath} - ${keyword} - ${message} - ${data}`);
+});
+
+process.exit(1);
