76fd855822d37aaaaa67dea3b7b31da8397cfa50
Sayali Jagtap <jagtapsayali0304@gmail.com>
2018-07-26 10:58:28 +0530

    Created new orc test-runner

---
+++ orcs/test-runner/task.js
@@ -0,0 +1,12 @@
+const shell = require("shelljs");
+shell.config.silent = true;
+
+export default function(data) {
+    return new Promise(function(resolve, reject) {
+        try {
+            // write your code here
+        } catch(e) {
+            reject(e);
+        }
+    });
+}
