3ce522896a2cd149eaa858cd1e6d3171606d51d2
Sayali Jagtap <jagtapsayali0304@gmail.com>
2018-07-27 11:33:28 +0530

    Running mocha tests

--- orcs/test-runner/task.js
+++ orcs/test-runner/task.js
@@ -4,7 +4,8 @@ shell.config.silent = true;
 export default function(data) {
     return new Promise(function(resolve, reject) {
         try {
-            shell.cp('-R', "test-project/test", data.directory);
+            shell.cp("-R", "test-project/test", data.directory);
+            shell.exec("mocha", {cwd: data.directory});
         } catch(e) {
             reject(e);
         }
