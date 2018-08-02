4f10ed78dbaca75b55e90c6046b71f155e9e0997
Mahesh Kumar Kolla <mahesh.mk.245@gmail.com>
2018-08-02 20:49:11 +0530

    Installing mocha and its test-reporter while building the docker image

--- orcs/test-runner/index.js
+++ orcs/test-runner/index.js
@@ -5,12 +5,4 @@ import task from "./task";
 import config from "./config";
 const orcRunner = require("orc-runner");
 
-console.log("Installing mocha & mocha json reporter globally");
-shell.exec("npm install -g mocha", function(){
-  console.log("Installed mocha");
-});
-shell.exec("npm install -g git+https://github.com/sayalija/mocha-json-reporter.git", function(){
-  console.log("Installed mocha json reporter");
-});
-
 orcRunner.start(config, task);
