c35e766a5d23b76c7bc24644c695704eb70526f5
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2018-08-10 16:21:43 +0530

    Added the ability to pick up tests/files/configs based on the project

--- orcs/test-runner/task.js
+++ orcs/test-runner/task.js
@@ -1,22 +1,24 @@
 const shell = require("shelljs");
 const fs = require("fs");
 shell.config.silent = true;
-const TESTS_PATH = "./test-project/test";
+const TESTS_ROOT = process.env.TESTS_ROOT || "./test-project";
 
-export default function(data) {
+export default function(data,config) {
     return new Promise(function(resolve, reject) {
         function copyTests(testsPath, codePath) {
-            shell.exec(`cp -R ${testsPath} ${codePath}`);
+            shell.exec(`cp -R ${testsPath}/* ${codePath}`);
         }
 
         function runTests(directory) {
-          shell.exec("mocha --reporter mocha_reporter", {cwd: directory});
+          shell.exec("mocha --reporter mocha_reporter __test", {cwd: directory});
           const resultFile = fs.readFileSync(`${directory}/__report.json`, "utf-8");
+          // copy log file as well
           return JSON.parse(resultFile);
         }
 
         try {
-            copyTests(TESTS_PATH, data.directory);
+            let testsPath = [TESTS_ROOT,project.repo_keyword].join("/");
+            copyTests(testsPath, data.directory);
             const report = runTests(data.directory);
             resolve(report);
         } catch(e) {
